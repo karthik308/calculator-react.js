69cb564eff83bf2675d23087eef15ce5833abeb6
karthik <vkarthik308@gmail.com>
2021-09-21 18:17:12 +0530

    feat: inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{useState} from "react";
+import './App.css';
+const App=()=>{
+    const [result, setResult]=useState("");
+    const handleClick=(e)=>{
+        setResult(result.concat(e.target.name));
+    }
+    const backspace =()=>{
+        setResult(result.slice(0,result.length - 1))
+
+    }
+    const clear =()=>{
+        setResult("");
+    }
+    const calculate=()=>{
+        try{
+            setResult(eval(result).toString())
+
+        }
+        catch(error){
+            setResult('SyntaxError')
+        }
+    }
+    return(
+        
+        <div>
+        <div className='page-head' >
+          <p>  Find a easy way to do calculation</p>
+           </div>
+        <div className="container">
+            <form>
+                <input type="text" value={result} />
+            </form>
+            <div className="keys">
+              <button className='functionalkeys' onClick={clear} id="clear">Clear</button>
+              <button  className='functionalkeys' onClick={backspace} id="backspace">C</button>
+              <button  className='functionalkeys'name="/" onClick={handleClick} >&divide;</button> 
+              <button name="7" onClick={handleClick}>7</button>
+              <button name="8" onClick={handleClick}>8</button>
+              <button name="9" onClick={handleClick}>9</button>
+              <button   className='functionalkeys'name="*" onClick={handleClick}>&times;</button>
+              <button name="4" onClick={handleClick}>4</button>
+              <button name="5" onClick={handleClick}>5</button>
+              <button name="6" onClick={handleClick}>6</button>
+              <button  className='functionalkeys' name="-" onClick={handleClick}>&ndash;</button>
+              <button name="1" onClick={handleClick}>1</button>
+              <button name="2" onClick={handleClick}>2</button>
+              <button name="3" onClick={handleClick}>3</button>
+              <button  className='functionalkeys' name="+" onClick={handleClick}>+</button>
+              <button name="0" onClick={handleClick}>0</button>
+              <button name="." onClick={handleClick}>.</button>
+              <button  className='functionalkeys'name="=" onClick={calculate} id='result'>=</button>
+          
+            </div>
+        </div>
+        
+        </div>
+       
+        
+    )
 }
+
 
 export default App;
